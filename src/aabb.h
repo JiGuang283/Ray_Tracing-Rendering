f304a1a3f20e5f92099e4d688919121da4ced1d9
JiGuang283 <wangdch23@mail2.sysu.edu.cn>
2025-12-09 08:32:54 +0800

    Optimize ray-AABB intersection by precomputing inverse direction and sign in ray class.

--- src/aabb.h
+++ src/aabb.h
@@ -29,11 +29,13 @@ class aabb {
 };
 
 inline bool aabb::hit(const ray &r, double t_min, double t_max) const {
+    const vec3 &inv_dir = r.inv_direction();
+    const int *sign = r.direction_sign();
+
     for (int a = 0; a < 3; a++) {
-        auto invD = 1.0f / r.direction()[a];
-        auto t0 = (min()[a] - r.origin()[a]) * invD;
-        auto t1 = (max()[a] - r.origin()[a]) * invD;
-        if (invD < 0.0f) {
+        auto t0 = (min()[a] - r.origin()[a]) * inv_dir[a];
+        auto t1 = (max()[a] - r.origin()[a]) * inv_dir[a];
+        if (sign[a]) {
             std::swap(t0, t1);
         }
         t_min = t0 > t_min ? t0 : t_min;
--- src/ray.h
+++ src/ray.h
@@ -8,6 +8,11 @@ class ray {
     ray() = default;
     ray(const point3 &origin, const vec3 &direction, double time = 0.0) noexcept
         : orig(origin), dir(direction), tm(time) {
+        inv_dir =
+            vec3(1.0 / direction.x(), 1.0 / direction.y(), 1.0 / direction.z());
+        dir_sign[0] = (inv_dir.x() < 0);
+        dir_sign[1] = (inv_dir.y() < 0);
+        dir_sign[2] = (inv_dir.z() < 0);
     }
 
     point3 origin() const noexcept {
@@ -15,6 +20,12 @@ class ray {
     }
     vec3 direction() const noexcept {
         return dir;
+    }
+    vec3 inv_direction() const noexcept {
+        return inv_dir;
+    }
+    const int *direction_sign() const noexcept {
+        return dir_sign;
     }
     double time() const noexcept {
         return tm;
@@ -27,6 +38,8 @@ class ray {
   private:
     point3 orig;
     vec3 dir;
+    vec3 inv_dir;
+    int dir_sign[3];
     double tm = 0.0;
 };
 
